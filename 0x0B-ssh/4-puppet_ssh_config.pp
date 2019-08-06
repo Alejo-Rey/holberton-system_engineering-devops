88247584fa1e6b2e3a71f4699f845f99b8e50660
Alejo-rey <771@holbertonschool.com>
2019-08-06 21:04:21 +0000

    [feature] create a puppet to change config

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # Puppet to create a file and give permition
 exec { 'add_lines':
-  command => '/bin/echo -e "    PasswordAuthentication No\n    IdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
-  path    => '/etc/bin'
+  command  => 'echo -e "    PasswordAuthentication no\n    IdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config',
+  provider => 'shell'
 }
