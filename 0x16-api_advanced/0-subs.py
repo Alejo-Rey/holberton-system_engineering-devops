dc8cb5b74ebfe623a0a2bad89d3ec351226b959f
alejo-rey <771@holbertonschool.com>
2019-09-23 02:53:59 +0000

    [FEATURE] write a script that queries the API and returns de subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,8 @@ def number_of_subscribers(subreddit):
     """ function to get the subcribers """
     url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
     headers = {"User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0)\
-            Gecko/20100101 Firefox/69.0"}
+               "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0)\
+               Gecko/20100101 Firefox/69.0"}
     req = requests.get(url, headers=headers, allow_redirects=False)
     if req.status_code == 200:
         return req.json().get('data').get('subscribers')
