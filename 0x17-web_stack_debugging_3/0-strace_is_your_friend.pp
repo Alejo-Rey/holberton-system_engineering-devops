68246b0c424e5eb40738e4ba41e976582cfb7e0c
Alejo-rey <771@holbertonschool.com>
2019-10-03 03:59:26 +0000

    [FIX] semicolons

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 exec { '/var/www/html/wp-settings.php':
-  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  provider => shell,
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  provider => 'shell'
 }
